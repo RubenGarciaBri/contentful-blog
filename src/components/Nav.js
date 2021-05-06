a9cb3b73a586fd9f453f4fddafc6f985500ed816
Ruben <hello@rubengarciabri.me>
2021-05-06 23:53:27 +0100

    change postCard button font size

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link, graphql } from "gatsby"
+import logo from '../images/logo1.png'
 
 const Nav = () => {
   return (
     <nav className="main-nav container">
       <div className="logo">
-        {/* <Link href="index.html"><img src="img/logo1.png" alt="logo"/></Link> */}
+        <Link href="/"><img src={logo} alt="logo"/></Link>
       </div>
       <ul className="navBar">
-        <li className="navBar__item"><Link to="/">Home</Link></li>
+        {/* <li className="navBar__item"><Link to="/">Home</Link></li>
         <li className="navBar__item"><Link to="/blog">Blog</Link></li>
-        <li className="navBar__item"><Link to="/contact">Get In Touch</Link></li>
+        <li className="navBar__item"><Link to="/contact">Get In Touch</Link></li> */}
       </ul>
     </nav>
   )
